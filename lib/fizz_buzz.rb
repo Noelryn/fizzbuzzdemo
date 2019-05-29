398b8b54313919b39783a1d4260f7eeba10c07b4
Noel Ryan <noelgerard.ryan@gmail.com>
2019-05-29 15:44:35 +0200

    Adding test for buzz to return if 5 is entered, all test currently passing

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,9 @@
 
 def fizz_buzz(number)
-        if number %3 == 0
-            'Fizz!'
+    if number %3 == 0
+        'Fizz!'
+    elsif number %5 == 0
+        'Buzz!'
     else
         number
     end
