bd724107652cf7aea866e357f432699f336570a7
Noel Ryan <noelgerard.ryan@gmail.com>
2019-05-29 15:53:37 +0200

    adding in fizzbuzz if number is 15 tests into spec file and changed order of code in fizzbuzz.rb so all test passing

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -14,4 +14,8 @@ describe 'fizz_buzz' do
         expect(fizz_buzz(5)).to eq 'Buzz!'
     end
 
+    it 'returns FizzBuzz!! if number is divisable 15' do
+        expect(fizz_buzz(15)).to eq 'FizzBuzz!!'
+    end
+
 end
